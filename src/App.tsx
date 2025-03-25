d3c5b571bb864677c09a65bf5942e2f172f54483
Dandelion <114991717+DandelionWithLute@users.noreply.github.com>
2025-03-25 19:10:22 +0800

    scope set up

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { appDataDir } from "@tauri-apps/api/path";
+import { readDir, BaseDirectory } from "@tauri-apps/plugin-fs";
+import { dataDir } from "@tauri-apps/api/path";
 
 function App() {
   // tauriInitialization
+  // https://v2.tauri.app/plugin/file-system/#scopes
   const [AppDataDir, setAppDataDir] = useState("");
+  const [ReadDir, setReadDir] = useState("");
+  const [DataDir, setDataDir] = useState("");
   // sth else...
+  const [currentDir, setCurrentDir] = useState("");
 
   async function tauriInitialization() {
     const appDataDirPath = await appDataDir();
     setAppDataDir(appDataDirPath);
+    setCurrentDir(appDataDirPath);
+    const entries = await readDir("/", {
+      baseDir: BaseDirectory.AppLocalData,
+    });
+    console.log(entries, BaseDirectory.AppLocalData);
+
+    const dataDirPath = await dataDir();
+    setDataDir(dataDirPath);
   }
 
   useEffect(() => {
@@ -19,7 +33,9 @@ function App() {
   return (
     <div>
       <p>123</p>
-      {AppDataDir}
+      <div className="text-red-500">{AppDataDir}</div>
+      <div>{ReadDir}</div>
+      <div>{DataDir}</div>
     </div>
   );
 }
